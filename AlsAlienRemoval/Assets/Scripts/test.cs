816c2e6c749b8612fcf2936cea4cd0caaa2012e2
Benjamin Collicott <55672347+BCollicott@users.noreply.github.com>
2020-09-14 18:06:16 -0400

    Ben test commit
    added console message to test script
    added test script as component to main camera

--- AlsAlienRemoval/Assets/Scripts/test.cs
+++ AlsAlienRemoval/Assets/Scripts/test.cs
@@ -7,7 +7,7 @@ public class test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Alien Removal is the process of removing aliens from a location");
     }
 
     // Update is called once per frame
