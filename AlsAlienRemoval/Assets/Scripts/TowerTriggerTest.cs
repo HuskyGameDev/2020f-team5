1c14374db80ecb03f39a8d0fb1bcf51ae2b18fe1
Benjamin Collicott <55672347+BCollicott@users.noreply.github.com>
2020-09-18 11:27:54 -0400

    Added Tilemap collisions w/ debug objects
    -Tilemap now has collisions for "grass" non-path tiles
    -Added physics ball to show collisions
    -Added placeholder tower to show trigger interaction w/ non-path tiles, AKA where towers can be placed

---
+++ AlsAlienRemoval/Assets/Scripts/TowerTriggerTest.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerTriggerTest : MonoBehaviour
+{
+    private SpriteRenderer _tileHighlight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpriteRenderer[] spriteRenderers = GetComponentsInChildren<SpriteRenderer>();   // All spriterenderers in this gameobject and children
+
+        // Find spriterenderer of tilehighlight (spriterenderer belonging to only child of this gameobject)
+        foreach (SpriteRenderer s in spriteRenderers)
+        {
+            if (s.gameObject.transform.parent == this.transform)
+            {
+                _tileHighlight = s;
+                break;
+            } 
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+       // _tileHighlight.transform.position = new Vector2(Mathf.Round(transform.position.x - 1), Mathf.Round(transform.position.y - 1));
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        _tileHighlight.color = Color.green;
+        Debug.Log("Enter!");
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        _tileHighlight.color = Color.red;
+        Debug.Log("Exit!");
+    }
+}
