eb6769c6af43201da285e487e0fd3751b2e0427e
npromero <61207227+npromero@users.noreply.github.com>
2020-09-21 17:02:54 -0400

    tower spawning
    added a button to spawn towers

---
+++ AlsAlienRemoval/Assets/Scripts/buildingPlacement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buildingPlacement : MonoBehaviour
+{
+    private Transform currentBuilding;
+    private bool hasPlaced;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+        if(currentBuilding != null && !hasPlaced)
+            {
+            Vector2 m = Input.mousePosition;
+            currentBuilding.position = new Vector2(m.x/100,m.y/100);
+            if(Input.GetMouseButtonDown(0))
+                {
+                hasPlaced = true;
+            }
+            if(hasPlaced)
+                {
+                currentBuilding = null;
+            }
+        }
+        
+    }
+    public void setItem(GameObject b)
+        {
+        hasPlaced = false;
+       currentBuilding = ((GameObject)Instantiate(b)).transform;
+        }
+}
