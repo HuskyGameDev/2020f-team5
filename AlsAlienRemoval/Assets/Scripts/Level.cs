e285dd40e29f83bf35c61a2d318b6d6e0ce23c91
Benjamin Collicott <55672347+BCollicott@users.noreply.github.com>
2020-09-29 13:12:03 -0400

    Implemented existing art assets
    -Added enemy and tower sprites from game design document
    -Created 3 enemy prefabs for existing types. Behavior no different from previous debug enemy
    -Debug enemy spawning now spawns random enemy types instead of random generated enemies

--- AlsAlienRemoval/Assets/Scripts/Level.cs
+++ AlsAlienRemoval/Assets/Scripts/Level.cs
@@ -9,19 +9,23 @@ public class Level : MonoBehaviour
     public Enemy debugEnemy;
 
     public bool doDebugSpawning;            // Spawn random enemies for debug purposes
+    public Enemy debugEnemy1;
+    public Enemy debugEnemy2;
+    public Enemy debugEnemy3;
 
+    private Enemy[] _debugEnemies;
     private float _spawnTimer = 0f;         // Time since last spawn (debug)
     private float _spawnCooldown = .25f;    // Time required between spawning enemies (debug)
 
+    private void Start()
+    {
+        _debugEnemies = new Enemy[] { debugEnemy1, debugEnemy2, debugEnemy3 };
+    }
     public void spawnEnemy (Enemy enemyType)
     {
         // Create new enemy of specified type at spawnpoint and set its destination to first waypoint
         Enemy newEnemy = GameObject.Instantiate(enemyType, spawnpoint.transform.position, Quaternion.identity);
 
-        // FOR NOW: Give enemy random, but proportional speed and size stats
-        float enemyScalar = Random.Range(1f, 4f);   // Speed/size scale
-        newEnemy.speed *= enemyScalar;
-        newEnemy.transform.localScale *= (5f - enemyScalar);
         firstWaypointArea.setAsNextDestination(newEnemy);
     }
 
@@ -34,7 +38,9 @@ public class Level : MonoBehaviour
 
             if (_spawnTimer >= _spawnCooldown)
             {
-                spawnEnemy(debugEnemy);
+                int rand = (int)Random.Range(0f, 2.99f);
+                Debug.Log(rand);
+                spawnEnemy(_debugEnemies[rand]);
                 _spawnTimer %= _spawnCooldown;
             }
         }
