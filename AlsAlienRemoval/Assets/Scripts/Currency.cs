e3b4cb15176387e8942e47e15ebb36f2b6e09cee
npromero <61207227+npromero@users.noreply.github.com>
2020-10-26 16:38:35 -0400

    Started the basis of the currency.
    Made a text that can be changed from a script so the value can change

---
+++ AlsAlienRemoval/Assets/Scripts/Currency.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Currency : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+    Text currency = GameObject.Find("Canvas/MainUIPanel/CurrencyDisplay").GetComponent<Text>();
+    currency.text = "500$";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void TextChange()
+    {
+        //currency.text ="500$" ;
+    }
+}
