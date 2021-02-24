456f3057b82829f275943883f30312be12c5df3e
Benjamin Collicott <55672347+BCollicott@users.noreply.github.com>
2021-02-23 21:42:11 -0500

    Added wave start button
    -Wave countdown/start now controlled by UI button
    -Level fixedUpdate() function greatly simplified,
    -Timer code moved to separate script attached to timer text

--- AlsAlienRemoval/Assets/Scripts/Level.cs
+++ AlsAlienRemoval/Assets/Scripts/Level.cs
@@ -41,7 +41,9 @@ public class Level : MonoBehaviour
     private Text _waveNumber;                   // Wave number text
     private Text _enemiesRemaining;             // Enemies remaining text
     private Text _livestockRemaining;           // Livestock remaining text
-    private WaveTimer _waveTimer;
+    public WaveTimer waveTimer;
+
+    private Button _utilityButton;
 
     // building error message fader stuff
     private GameObject no_money_text;
@@ -70,8 +72,8 @@ public class Level : MonoBehaviour
         spawnCows();
 
         // timer stuff
-        _waveTimer = GameObject.Find("TopUIPanel").AddComponent<WaveTimer>();
-        _waveTimer.enabled = false;
+        _utilityButton = GameObject.Find("btn_utility").GetComponent<Button>();
+        waveTimer.enabled = false;
         _waveInProgress = false;
 
         no_money_text = GameObject.Find("text_no_money");
@@ -107,36 +109,39 @@ public class Level : MonoBehaviour
         // spawns wave enemies if "debug spawning" is enabled
         if (doDebugSpawning) {
 
-            // begins the wave
-            if (!_waveInProgress) {
-                _waveInProgress = true;
-                _spawningInProgress = true;
-                _strengthLeftToSpawn = _waveStrengths[WaveNumber - 1];
-            }
-
-            // fills spawn
-            if (_spawningInProgress) {
+            // fills spawn if timer is completed and spawning still needed
+            if (_spawningInProgress && !waveTimer.enabled) {
                 SpawnWaveChunk();
             }
 
-            // wave cleared, start timer
-            if (!_waveTimer.enabled && _waveInProgress && EnemiesRemaining == 0) {
-                _waveTimer.enabled = true;
+            // wave cleared, enable button for timer starting
+            if (!waveTimer.enabled && _waveInProgress && EnemiesRemaining == 0) {
+                _utilityButton.interactable = true;
+                _waveInProgress = false;
                 Debug.Log($"WAVE {WaveNumber} DEFEATED");
                 if (WaveNumber == 10)
                 {
                     SceneManager.LoadScene("Win Screen");
                 }
-            }
 
-            // timer reaches 0, spawn next wave
-            if (_waveTimer.enabled && _waveTimer.timeRemaining <= 0) {
                 WaveNumber++;
-                Debug.Log($"WAVE {WaveNumber} BEGINS");
-                _waveTimer.enabled = false;
-                _waveInProgress = false;
             }
+            
+        }
+    }
 
+    public void startWave()
+    {
+        if (!_waveInProgress)
+        {
+            waveTimer.enabled = true;
+            _utilityButton.interactable = false;
+
+            _waveInProgress = true;
+            _spawningInProgress = true;
+            _strengthLeftToSpawn = _waveStrengths[WaveNumber - 1];
+
+            Debug.Log($"WAVE {WaveNumber} BEGINS");
         }
     }
 
@@ -184,49 +189,5 @@ public class Level : MonoBehaviour
         }
     }
 
-    // timer class for displaying the time until the next wave
-    private class WaveTimer : MonoBehaviour {
-
-        Text _timerText;                    // timer text on the main ui panel
-        Text _labelText;                    // label text "Next wave:"
-        public float timeRemaining;         // time remaining on this timer
-        
-        private void Awake() {
-            
-            // retrieves timer crap
-            _timerText = GameObject.Find("next_wave_timer").GetComponent<Text>();
-            _labelText = GameObject.Find("next_wave_timer_label").GetComponent<Text>();
-            _timerText.enabled = false;
-            _labelText.enabled = false;
-        }
-
-        // enables the timer text, sets to 10s
-        void OnEnable() {
-
-            // todo: make this value dynamic
-            timeRemaining = 5;
-
-            _timerText.enabled = true;
-            _labelText.enabled = true;
-        }
-
-        // disables the timer tex
-        void OnDisable() {
-            _timerText.enabled = false;
-            _labelText.enabled = false;
-        }
-
-        // updates time text. self-disables when it reaches 0
-        void FixedUpdate() {
-            if (timeRemaining > 0) {
-                timeRemaining -= Time.fixedDeltaTime;
-                if (timeRemaining < 0)
-                    timeRemaining = 0;
-                _timerText.text = timeRemaining.ToString("0.0");
-            }
-            else {
-                this.enabled = false;
-            }
-        }
-    }
+    
 }
--- AlsAlienRemoval/Assets/Scripts/WinMenu.cs
+++ AlsAlienRemoval/Assets/Scripts/WinMenu.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
 public class WinMenu : MonoBehaviour
 {
     public void StartAgain()
